cb4d93104dd75717eb61dd426eb2cd28d3bfe6bc
tirush1245 <108980519+tirush1245@users.noreply.github.com>
2022-11-12 15:16:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,16 +52,16 @@ resource "aws_subnet" "subnet2-public" {
     }
 }
 
-resource "aws_subnet" "subnet3-public" {
-    vpc_id = "${aws_vpc.default.id}"
-    cidr_block = "${var.public_subnet3_cidr}"
-    availability_zone = "us-east-1c"
+# resource "aws_subnet" "subnet3-public" {
+#     vpc_id = "${aws_vpc.default.id}"
+#     cidr_block = "${var.public_subnet3_cidr}"
+#     availability_zone = "us-east-1c"
 
-    tags = {
-        Name = "${var.public_subnet3_name}"
-    }
+#     tags = {
+#         Name = "${var.public_subnet3_name}"
+#     }
 	
-}
+# }
 
 
 resource "aws_route_table" "terraform-public" {
