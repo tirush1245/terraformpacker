1cab5e5573a710d625fe89d44f5a1ccd2cf2bf26
tirush1245 <108980519+tirush1245@users.noreply.github.com>
2022-11-12 15:41:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ resource "aws_internet_gateway" "default" {
 resource "aws_subnet" "subnet1-public" {
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_subnet1_cidr}"
-    availability_zone = "us-east-1a"
+    availability_zone = "ap-south-1a"
 
     tags = {
         Name = "${var.public_subnet1_name}"
@@ -45,7 +45,7 @@ resource "aws_subnet" "subnet1-public" {
 resource "aws_subnet" "subnet2-public" {
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_subnet2_cidr}"
-    availability_zone = "us-east-1b"
+    availability_zone = "ap-south-1b"
 
     tags = {
         Name = "${var.public_subnet2_name}"
